917231da5e1dde75fcd35058996fb77786b05c58
Kaffo <kaffse@gmail.com>
2017-04-22 15:26:36 +0100

    Added simple drop. Terrible locomotion attempt on world

--- Assets/LD/Scripts/VrGrabbable.cs
+++ Assets/LD/Scripts/VrGrabbable.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VrGrabbable : MonoBehaviour {
-
+    public ControllerManager controllerman;
     private Transform controller = null;
     private FixedJoint joint;
 
-	// Use this for initialization
-	void Start () {
+    private SteamVR_Controller.Device device
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)controller.GetComponent<SteamVR_TrackedObject>().index);
+        }
+    }
+    // Use this for initialization
+    void Start () {
         joint = transform.parent.GetComponent<FixedJoint>();
         if (joint == null)
         {
@@ -18,7 +25,14 @@ public class VrGrabbable : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (controller)
+        {
+            if (device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip))
+            {
+                //Debug.Log("Drop");
+                drop();
+            }
+        }
 	}
 
     public void destory()
@@ -30,6 +44,24 @@ public class VrGrabbable : MonoBehaviour {
         Destroy(transform.parent.gameObject);
     }
 
+    public void drop()
+    {
+        if (controller)
+        {
+            controller.tag = "Controller";
+            joint.connectedBody = null;
+            StartCoroutine("delayDrop");
+        }
+    }
+
+    private IEnumerator delayDrop()
+    {
+        //Debug.Log("Wait");
+        yield return new WaitForSeconds(0.5f);
+        //Debug.Log("Detach");
+        controller = null;
+        yield return null;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Controller") && controller == null)
