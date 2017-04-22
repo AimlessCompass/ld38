04edc7641b124993ace4c12773dd22f4a6a23b5f
Keir Smith <Keir Smith>
2017-04-22 12:32:33 +0100

    Keycard prototype

---
+++ Assets/LD/Scripts/ReaderController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaderController : MonoBehaviour {
+    public Transform door;
+
+    private bool openDoor = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (openDoor)
+        {
+            door.Translate(0f, 0.01f, 0f);
+            if (door.position.y > (door.localScale.y / 2 + door.localScale.y))
+            {
+                openDoor = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Key"))
+        {
+            openDoor = true;
+            other.transform.GetComponent<VrGrabbable>().destory();
+        }
+    }
+}
---
+++ Assets/LD/Scripts/VrGrabbable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrGrabbable : MonoBehaviour {
+
+    private Transform controller = null;
+    private FixedJoint joint;
+
+	// Use this for initialization
+	void Start () {
+        joint = transform.parent.GetComponent<FixedJoint>();
+        if (joint == null)
+        {
+            Debug.Log("Joint not found!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void destory()
+    {
+        if (controller)
+        {
+            controller.tag = "Controller";
+        }
+        Destroy(transform.parent.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Controller") && controller == null)
+        {
+            Debug.Log("Attached");
+            controller = other.transform;
+            controller.tag = "ControllerGrabbed";
+            joint.connectedBody = controller.GetComponent<Rigidbody>();
+        }
+    }
+}
