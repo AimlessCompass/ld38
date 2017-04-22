ba9dfa21e754c42174d9adaae45637ec57feb606
Kaffo <kaffse@gmail.com>
2017-04-22 21:18:30 +0100

    Added basic wall collision for #1

---
+++ Assets/LD/Scripts/HeadController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform otherTrans = collision.transform;
+        //Debug.Log("Collide");
+        if (otherTrans.CompareTag("Wall"))
+        {
+            //Debug.Log(collision.relativeVelocity);
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Vector3 pushBack = (transform.position - contact.point) * 0.1f;
+            //player.Translate(new Vector3(pushBack.x, 0f, pushBack.y));
+            player.Translate(contact.normal * 0.01f);
+            Debug.DrawRay(contact.point, new Vector3(pushBack.x, 0f, pushBack.y), Color.blue, 20f);
+        }
+    }
+}
